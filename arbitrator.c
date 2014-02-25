a2a6cc4251cd9fa42b519d6e27dee560121f4ac3
vennard <vennard@wisc.edu>
2014-02-25 02:39:47 -0600

    working readin

--- arbitrator.c
+++ arbitrator.c
@@ -17,58 +17,56 @@
 // Player Num = 1 -> 4
 // Orders: 0 - Hold, 1 - Move, 2 - Support, 3 - Convoy
 // BY DEFAULT 000 = NULL (represents no country)
-void
-usage(char *prog)
-{
-   fprintf(stderr, "usage: %s <-i input game data file> <-o input orders file>\n", prog);
-   exit(1);
-}
-
-int
-main(int argc, char *argv[])
-{
-  genOrders(2,2,"/tmp/outfile");
-  genStart("/tmp/startdata");
-
-  printf("Test the global pointers...\r\n");
 
 
-
-
-// Setup Board -- TODO make legit tmp files
-//   char *inFile   = "/no/such/file";
-//char *inOrders = "/no/such/thing";
-/*
-   // input params
-   int inparms;
-   opterr = 0;
-   while ((inparms = getopt(argc, argv, "i:o:")) != -1) {
-switch (inparms) {
-case 'i':
-   inFile = strdup(optarg);
-   break;
-case 'o':
-   inOrders = strdup(optarg);
-   break;
-default:
-   usage(argv[0]);
-}
-  }
-
-   // open game start data file
-   int fd = open(inFile, O_RDONLY);
+region_t g[48];
+char fgdata[] = {"/tmp/gamedata"};
+char forders[] = {"/tmp/outfile"};
+int loadgamedata(char f[]) {
+   int fd = open(f, O_RDONLY);
    if (fd < 0) {
-  perror("read");
-  exit(1);
+  		perror("read");
+  		exit(1);
    }
+	if (read(fd,g,sizeof(g)) < 0) perror("read");
+	printf("Read success, first 35 country code = %d\r\n",g[35].name);
+	return 0;
+}
 
-// open game orders file
-   int fd1 = open(inOrders, O_RDONLY);
+int startnewgame(int num) {
+	printf("Starting new game...");
+  	//just handling one save game -- case statement for more
+	char fname[] = "/tmp/savegame";
+	if (num == 0) {
+	  //fname = "/tmp/savegame1";
+	} else {
+		return 1;	
+	}
+	printf(" saved as: %s\r\n",fname);
+	genStart(fname); //MUST BE CHANGED TO PERMANENT LOCATION
+	loadgamedata(fname);
+	return 0;
+}
+  
+
+int main(int argc, char *argv[]) {
+	genOrders(2,2,forders);
+	genStart(fgdata);
+	printf("Start of Testing...\r\n");
+	startnewgame(0);
+
+	
+
+	/*
+	// open game orders file
+   int fd1 = open(forders, O_RDONLY);
    if (fd1 < 0) {
-  perror("read");
-  exit(1);
+  		perror("read");
+  		exit(1);
    }
+	*/
 
-*/
+
+
    return 0;
 }
