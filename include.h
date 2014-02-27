3de5e3c925575b440e19439368911bf32713ef50
John <vennard@wisc.edu>
2014-02-27 01:10:30 -0600

    cleaned up main code 2

---
+++ include.h
@@ -0,0 +1,16 @@
+#ifndef __include_h__
+#define __include_h__
+
+#define MAX_ORDERS 20000
+
+extern region_t g[48];
+extern order_t o[MAX_ORDERS];
+extern int validOrders;
+extern int numO;
+
+extern int loadgamedata(char f[]);
+extern int startnewgame(int num);
+extern int getTestOrders(int seed,int numOrders, char f[]);
+extern int firstvalidate(void);
+
+#endif
