1ac2b32713bcba834eab424e3f3a65b7dffbde51
John <vennard@wisc.edu>
2014-02-28 01:16:07 -0600

    starting smart ordering system

--- testorders.c
+++ testorders.c
@@ -7,7 +7,9 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "region.h"
 #include "order.h" //see file for definitions
+#include "include.h"
 //Creating sample output from the controllers
 // Sending string (bytes)
 // Player Num (1) / Order (1) / Unit Location (3) / [To Country (3)]
@@ -15,15 +17,73 @@
 // Player Num = 1 -> 4
 // Orders: 0 - Hold, 1 - Move, 2 - Support, 3 - Convoy
 // BY DEFAULT -1 represents no country (invalid)
+char *outFile   = "/tmp/outfile";
+int ordersLeft = 0;
+int* ptr;
+int num[64];
+int *nptr = num;
+
+//search gameboard for players units of type
+//returns number of units found
+//0 - land, 1 - fleet
+int findunits(int player, int type) {
+    int c = 0;
+    int i;
+    for (i = 0;i < 48;i++) {
+        if ((g[i].player == player)&&(g[i].occupy_type == type)) {
+            num[c] = i;
+            c++;
+        }
+    }
+    return c;
+}
+//assumes gameboard is populated
+int iorder(int s, int num, char out[]) {
+    int a = 0;
+    order_t r;
+    ptr = (int *) malloc(sizeof(r));
+	printf("Launching iOrder -- your very own \"ahem\" intelligent ordering system. Processing %i Orders..\r\n",num);
+    if((num < 1)||(s < 0)||(strlen(out)<1)) return -1;
+    srand(s);
+    int k;
+    for (k = 0;k < num;k++) {
+        int *nc;
+        int a = 0;
+        int i = 0;
+        r.player = rand() % 4;
+        r.order = rand() % 4;
+        r.type = rand() % 2;
+        int numunits = findunits(r.player, r.type);
+        int x = rand() % numunits;
+        int country = nptr[x];
+        r.country = country;
+        switch (r.order) {
+            case 0 : //hold
+                break;
+            case 1 : //move
+                nc = g[country].ncountrys;
+                while(a != -1) {
+                    a = nc[i];
+                    i++;
+                }
+                int m = rand() % i;
+                r.tcountry = nc[m];
+                break; 
+            case 2 : //support
+                break;
+            case 3 : //convoy
+                break;
+        }
+    }
+
+    return 0;
+}
 
 int genOrders(int seed, int numOrders, char out[]) {
-   char *outFile   = "/tmp/outfile";
-   int ordersLeft = 0;
 	int sd = 0;
-   int* ptr;
 	order_t r;
 
-   ptr = (int *) malloc(sizeof(r));
+    ptr = (int *) malloc(sizeof(r));
 	printf("Generating %i Orders..\r\n",numOrders);
 
 	if (strlen(out) < 1) {
